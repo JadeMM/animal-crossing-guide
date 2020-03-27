04b2bcffc1761ce0a42911e36793e401c46b6dd4
JadeMM <jade.moksness@gmail.com>
2020-03-27 11:42:44 -0700

    Updates

---
+++ precache-manifest.2c084b9f4b46dc0f8329cfe5c00a406b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82179f2e77191917141253ded2a91164",
+    "url": "/animal-crossing-guide/index.html"
+  },
+  {
+    "revision": "9a1a3c34c2a31d760d71",
+    "url": "/animal-crossing-guide/static/css/main.fd337327.chunk.css"
+  },
+  {
+    "revision": "dc1dd338b44b0b25ba71",
+    "url": "/animal-crossing-guide/static/js/2.f6fd8ec2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animal-crossing-guide/static/js/2.f6fd8ec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a1a3c34c2a31d760d71",
+    "url": "/animal-crossing-guide/static/js/main.481d358e.chunk.js"
+  },
+  {
+    "revision": "bd08cb459419d96a02af",
+    "url": "/animal-crossing-guide/static/js/runtime-main.72c94438.js"
+  }
+]);
